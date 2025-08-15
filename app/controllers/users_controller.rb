0ea804a17fd2025f65c2bee73a156ad8a3acda0d
woarewe <rostislav.zhuravsky@gmail.com>
2025-08-15 09:57:40 +0200

    Finish MVP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
   def index
+    @users = User.order(id: :asc).all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @user.as_json
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
